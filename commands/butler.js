182b340187a7b0af2816e6c4112109e302438b72
Mishal <mishal.ajm@gmail.com>
2021-03-20 21:46:54 +0300

    Added gif capability

--- commands/butler.js
+++ commands/butler.js
@@ -1,9 +1,30 @@
-const replies = ["🤦‍♂️ ahhhh yes, what do you want cun... I mean Sir!", "Sir, what would you like","Sir, take this 👊", "Sir, would you like me to call your nanny 👶"];
-module.exports = (msg,args) => {
+
+const fetch = require('node-fetch');
+
+const replies = ["Ugh What do you want...Sir?🤦‍♂️", "Sir, what would you like","Sir, take this 👊", "Sir, would you like me to call your nanny 👶", "Sir, I'm sure you can wait", "Sir, your wish will remain a wish if you don't wait"];
+
+module.exports = async (msg,args) => {
     if (args.length === 0) {
         let index = Math.floor(Math.random() * replies.length);
         msg.channel.send(replies[index]);
-    } else if (args.length >= 1) {
-        
+    } 
+    // Take the command after !butler
+    let subcommand = args[0];
+    switch(subcommand) {
+        case "gif":
+            let keywords;
+            subcommand.length === 0 ? keywords = "annoyed" : keywords = args.join(' ');
+            console.log(keywords);
+            let url = `https://g.tenor.com/v1/search?&q=${keywords}&key=${process.env.TENORKEY}&limit=10&`;
+            let response = await fetch(url);
+            let json = await response.json();
+            let gif = json.results[0].url; 
+            msg.channel.send(gif);
+            msg.channel.send("GIF provided by Tenor");
+            break;
+        default:
+            break;
     }
+
+
 }
